f0cc65d050b60883c51e5876e3fc6f99e663fad7
Hipszki János <hipszki.janos@mxnet.hu>
2023-04-17 10:23:24 +0200

    initial commit

---
+++ src/test/java/eu/udemx/UdemxCarRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.udemx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemxCarRentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
